4764e2b3f150baee25150535ed13e1de8f0d03ec
cipherxbox <64415842+cipherxbox@users.noreply.github.com>
2020-11-26 20:20:02 +0100

    Update common.h

--- common.h
+++ common.h
@@ -1,4 +1,16 @@
+#pragma once
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
 #include <pthread.h>
+
 #include "xvpn.h"
